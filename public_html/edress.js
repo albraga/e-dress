3b92a84590b0b2ae362c4373c04d7b7ed4209c88
alexandre <alexandre@ect-virtualbox>
2014-07-16 09:58:59 -0300

    adicionado videoToCanvas

--- public_html/edress.js
+++ public_html/edress.js
@@ -1,3 +1,6 @@
+var video = document.querySelector('video');
+var canvas = document.querySelector('canvas');
+
 var EDress = {
     enableCamera: function() {
         navigator.getUserMedia = (navigator.getUserMedia ||
@@ -10,10 +13,10 @@ var EDress = {
                         video: true,
                         audio: false
                     },
-                    function(localMediaStream) {
-                        var video = document.querySelector('video');
-                        video.src = window.URL.createObjectURL(localMediaStream);                      
-                    },
+            function(localMediaStream) {
+
+                video.src = window.URL.createObjectURL(localMediaStream);
+            },
                     function(err) {
                         alert("Erro: " + err);
                     }
@@ -21,11 +24,23 @@ var EDress = {
         } else {
             alert("getUserMedia not supported");
         }
+    },
+    videoToCanvas: function() {
+        var context = canvas.getContext('2d');
+        video.addEventListener('play', function() {
+            draw(this, context, canvas.width, canvas.height);
+        }, false);
     }
 };
 
-document.addEventListener("DOMContentLoaded", function(){
+function draw(v, c, w, h) {
+    c.drawImage(v, 0, 0, w, h);
+    setTimeout(draw, 20, v, c, w, h);
+}
+
+document.addEventListener("DOMContentLoaded", function() {
     EDress.enableCamera();
+    EDress.videoToCanvas();
 });
 
 
